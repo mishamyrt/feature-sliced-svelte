45198f28aa8b18e399f651a7923ca7de7d7259d7
mishamyrt <misha@myrt.co>
2023-11-09 03:17:40 +0300

    test: add minimal guard test

---
+++ src/lib/utils/layers/guard.test.js
@@ -0,0 +1,9 @@
+import { describe, it, expect } from 'vitest';
+import { isCorrectName } from './guard.js';
+
+describe('layer guard test', () => {
+  it('resolves correct values', () => {
+    const result = isCorrectName('features/asd');
+    expect(result).toBeTruthy();
+  });
+});
