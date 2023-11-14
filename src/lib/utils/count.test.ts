2b9834731b7f651e813c3a2184b792116411a4f5
mishamyrt <misha@myrt.co>
2023-11-14 19:50:43 +0300

    test: update count test to match new "unique" logic

--- src/lib/utils/count.test.ts
+++ src/lib/utils/count.test.ts
@@ -5,20 +5,23 @@ import { getModulesCount, type ModulesCount } from './count.js'
 describe('getModulesCount', () => {
   beforeEach(() => {
     document.body.innerHTML = `
-      <div data-fsd data-fsd-layer="layer1"></div>
-      <div data-fsd data-fsd-layer="layer2"></div>
-      <div data-fsd data-fsd-layer="layer1"></div>
+      <div data-fsd="layer1/Feature1" data-fsd-layer="layer1"></div>
+      <div data-fsd="layer2/Feature1" data-fsd-layer="layer2"></div>
+      <div data-fsd="layer1/Feature2" data-fsd-layer="layer1"></div>
+      <div data-fsd="layer1/Feature2" data-fsd-layer="layer1"></div>
+      <div data-fsd="layer3/Feature1" data-fsd-layer="layer3"></div>
     `
   })
 
-  it('should return correct module count for each layer', () => {
+  it('should return correct unique module count for each layer', () => {
     const result: ModulesCount[] = getModulesCount()
-    expect(result).toHaveLength(2)
+    expect(result).toHaveLength(3)
 
     expect(result).toEqual(
       expect.arrayContaining([
         { name: 'layer1', count: 2 },
         { name: 'layer2', count: 1 },
+        { name: 'layer3', count: 1 },
       ]),
     )
   })
